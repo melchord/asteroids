245e2945174e593eba93399aa1443f5d3ba0d362
Melchor <mdmngz411@gmail.com>
2021-03-22 10:44:27 -0400

    - add coordinate conversion

--- main.py
+++ main.py
@@ -3,26 +3,28 @@ import pygame
 import math
 import random
 
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
 # initialize pygame and global variables
 pygame.init()
-width = 400;
-height = 300;
+width = 400
+height = 500
 screen = pygame.display.set_mode((width, height))
 done = False
-is_blue = True
+
+# convert cartesian coordinates to pygame coordinates
+def to_coord(coords):
+  return (width / 2 + coords[0], height/2 - coords[1])
 
 # draw function
 def draw():
-  if is_blue: color = (0, 128, 255)
-  else: color = (255, 100, 0)
-  pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
-  pygame.draw.circle(screen, (255, 255, 255), (width/2, height/2), 30)
+  pygame.draw.lines(screen, WHITE, True, [to_coord([0, -18]), to_coord([18, -24])])
 
 # Events
 while not done:
   for event in pygame.event.get():
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-      is_blue = not is_blue
     if event.type == pygame.QUIT:
       done = True
   draw()
