d7b3b316198fd705e35093c55b0e472a9e4c0c61
Melchor <mdmngz411@gmail.com>
2021-03-22 17:46:19 -0400

    update README

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
+# Pygame Asteroids
+
 This is a replication of the game Asteroids using Pygame.
 
-# Prerequisites:
+## Prerequisites:
 
 1. Python 3.9.2
 2. Pygame 2.0.1
+
+## Run the Project Locally:
+
+### Step 1: Clone the project
+
+```sh
+git clone https://github.com/melchord/asteroids
+cd asteroids
+```
+
+### Step 2: Run the main file
+
+```sh
+py main.py
+```
+
+This will open a seperate window which will run the game.
--- main.py
+++ main.py
@@ -3,29 +3,84 @@ import pygame
 import math
 import random
 
+from pygame.font import SysFont
+
+# initialize pygame
+pygame.init()
+
 # colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# initialize pygame and global variables
-pygame.init()
-width = 400
-height = 500
+#constants
+width = 1000
+height = 800
 screen = pygame.display.set_mode((width, height))
-done = False
+startingState = "Game";
 
 # convert cartesian coordinates to pygame coordinates
 def to_coord(coords):
   return (width / 2 + coords[0], height/2 - coords[1])
 
-# draw function
-def draw():
-  pygame.draw.lines(screen, WHITE, True, [to_coord([0, -18]), to_coord([18, -24])])
+def to_x(coord_x):
+  return width/2 + coord_x
 
-# Events
-while not done:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      done = True
-  draw()
-  pygame.display.flip()
+def to_y(coord_y):
+  return height/2 - coord_y
+
+# Draw text on the screen
+def draw_text(text, color, x ,y, text_size, center=True):
+  screen_text = pygame.font(SysFont("Calibri", text_size).render(text, True, color))
+  if center:
+    rect = screen_text.get_rect()
+    rect.center(to_x(x), to_y(y))
+  else:
+    rect = (to_x(x), to_y(y))
+  screen.blit(screen_text, rect)
+
+# TODO: Check for collisions
+def isColliding():
+  return 0
+
+
+# Create ship class
+class Ship:
+  def __init__(self):
+    self.angle = 0
+    self.rotate = 0
+  
+  def update_ship(self):
+    # TODO: Move Ship
+    # Rotate Ship
+    self.angle += self.rotate
+  
+  def draw_ship():
+    # Draw ship
+    pygame.draw.lines(screen, WHITE, True, [to_coord([0, -18]), to_coord([18, -24]), to_coord([0, 24]), to_coord([-18, -24])])
+
+def game():
+  # Init variables
+  game_state = startingState
+  ship_state = "Alive"
+  ship = Ship();
+
+  # Run Game
+  while game_state != "Exit":
+    # User Inputs
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        gameState = "Exit"
+    
+    # Draw Ship
+    if gameState != "Game Over":
+      ship.draw_ship()
+    else:
+      draw_text("Game Over", WHITE, 0, 0, 100)
+    pygame.display.flip()
+
+# State game
+game()
+
+# End game
+pygame.quit()
+quit()
